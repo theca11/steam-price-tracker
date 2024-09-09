55b0e4d7d9fa9609c1635a650a68ecdd90171e66
theca11 <48060851+theca11@users.noreply.github.com>
2024-09-09 17:42:04 +0200

    build: update rollup config, add license/changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.0] - 2024-09-09
+
+First release of Steam Price Tracker. The plugin displays the current price of any Steam app. Keep track of discounts and don't miss out on a bargain!
+
+- Automatically updates info every hour.
+- Press key to go to the Steam Store app page.
+- Long press key to force an update of all visible apps.
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,22 @@
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import path from 'node:path';
 import url from 'node:url';
-import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
+import license from 'rollup-plugin-license';
+
 
 const isWatching = !!process.env.ROLLUP_WATCH;
 const sdPlugin = 'dev.theca11.steam-price-tracker.sdPlugin';
+const banner = `/**
+* @overview Steam Price Tracker Stream Deck plugin
+* @author the_ca11
+* @license MIT
+* @copyright the_ca11 2024-${new Date().getFullYear()}
+*/`;
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -21,6 +30,7 @@ const pluginConfig = {
 			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath))
 				.href;
 		},
+		banner
 	},
 	external: ['sharp'],
 	plugins: [
@@ -52,6 +62,34 @@ const pluginConfig = {
 				});
 			},
 		},
+		copy({
+			targets: [
+				{ src: ['./LICENSE', './CHANGELOG.md'], dest: `${sdPlugin}` },
+			],
+			verbose: true,
+			copyOnce: isWatching,
+		}),
+		license({
+			thirdParty: {
+				includePrivate: false,
+				output: {
+					file: `${sdPlugin}/3rdparty-licenses.txt`,
+					template(dependencies) {
+						const sortedDeps = dependencies.sort((a, b) =>
+							a.name < b.name ? -1 : a.name > b.name ? 1 : 0,
+						);
+						return [
+							'This file lists third-party dependencies and their corresponding license notice.\n',
+							...sortedDeps.map(
+								(dep) =>
+									`${dep.name} (v${dep.version}) - ${dep.license}\n\n${dep.licenseText}`,
+							),
+						].join('\n--------------------\n\n');
+					},
+				},
+			},
+		}),
+
 	],
 };
 
@@ -64,6 +102,7 @@ const piConfig = {
 			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath))
 				.href;
 		},
+		banner
 	},
 	plugins: [
 		{
